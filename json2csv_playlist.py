3bb34ec85901aa1575909d3444e3f8951726351c
Diego Palacios <48029515+diegoopr@users.noreply.github.com>
2024-02-11 17:54:46 -0600

    V2.0
    Of course I went over the top and now it is able to convert all the important non-user JSON files that come when requesting for spotify data.
    Hope i am done now

--- json2csv_playlist.py
+++ json2csv_playlist.py
@@ -1,18 +1,100 @@
 import json
 import csv
+import os
 
-def json2csv(json_file, csv_file):
+def library_converter(json_file, csv_file):
     with open(json_file, 'r') as f:
         data = json.load(f)
 
+#Edit the fieldnames to separate the values by the headers of the json file
+#this one specifically is tailored for my spotify data files
 
-### Edit the fieldnames to separate the values by the headers of the json file
-### this one specifically is tailored for my spotify data files
-    with open(csv_file, 'w', newline = '') as f:
+#2.0 now opens the file in "append" mode (a) to keep on adding items even after reading
+#each file
+    with open(csv_file, 'a', newline = '') as f:
+        writer = csv.DictWriter(f, fieldnames=["type", "artist", "track_name", "album", "publisher", "uri"])
+        if has_headers(csv_file) == False: writer.writeheader()
+
+        for item in data["tracks"]:
+            content_type = "song"
+            artist_name = item["artist"]
+            song_name = item["track"]
+            album = item["album"]
+            uri = item["uri"]
+
+            writer.writerow({
+                "type": content_type,
+                "artist": artist_name,
+                "track_name": song_name,
+                "album": album,
+                "uri": uri,
+            })
+        
+        for item in data["albums"]:
+            content_type = "album"
+            artist_name = item["artist"]
+            album = item["album"]
+            uri = item["uri"]
+
+            writer.writerow({
+                "type": content_type,
+                "artist": artist_name,
+                "album": album,
+                "uri": uri,
+            })
+        
+        for item in data["shows"]:
+            content_type = "podcast"
+            name = item["name"]
+            publisher = item["publisher"]
+            uri = item["uri"]
+
+            writer.writerow({
+                "type": content_type,
+                "artist": name,
+                "publisher": publisher,
+                "uri": uri,
+            })
+
+        for item in data["episodes"]:
+            content_type = "episode"
+            name = item["name"]
+            show = item["show"]
+            uri = item["uri"]
+            
+            writer.writerow({
+                "type": content_type,
+                "artist": show,
+                "track_name": name,
+                "uri": uri,
+            })
+
+        for item in data["artists"]:
+            content_type = "artist"
+            artist = item["name"]
+            uri = item["uri"]
+            
+            writer.writerow({
+                "type": content_type,
+                "artist": artist,
+                "uri": uri,
+            })
+
+
+def playlist_converter(json_file, csv_file):
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+
+    with open(csv_file, 'a', newline = '') as f:
         writer = csv.DictWriter(f, fieldnames=["playlist_name", "last_modified_date", "song_name", "artist_name", "album_name", "track_uri", "added_date"])
-        writer.writeheader()
+        if has_headers(csv_file) == False: writer.writeheader()
 
-        for playlist in data["playlists"]:
+        playlists = data.get("playlists", [])
+        #kept getting a keyError when using
+        #for playlist in data["playlists"]
+        #so i had to handle it with "data.get"
+        
+        for playlist in playlists:
             playlist_name = playlist["name"]
             playlist_last_modified = playlist["lastModifiedDate"]
 
@@ -34,6 +116,73 @@ def json2csv(json_file, csv_file):
                     "added_date": added_date,
                 })
 
-###Add the path of the files to convert here, or run the program moving directly
-###the path where the json file is stored
-json2csv('Playlist1.json', 'playlist.csv')
+
+
+def history_converter(json_file, csv_file):
+    with open(json_file, 'r') as f:
+        data = json.load(f)
+
+    with open(csv_file, 'a', newline = '') as f:
+        writer = csv.DictWriter(f, fieldnames=["end_time", "artist_name", "track_name", "ms_played"])
+        if has_headers(csv_file) == False: writer.writeheader()
+        #writer.writerow(data)
+        #tried to work this one out without typing it all but it didnt work
+        #don't have enough time to look deeply into it
+        
+        for song in data:
+            end_time = song["endTime"]
+            artist = song["artistName"]
+            track_name = song["trackName"]
+            time_played = song["msPlayed"]
+
+            writer.writerow({
+                "end_time": end_time,
+                "artist_name": artist,
+                "track_name": track_name,
+                "ms_played": time_played,
+            })
+
+
+
+
+def has_headers(csv_file):
+    #reads the first row to know if there are headers
+    #I assume that if it is a new file, first row
+    #will be empty, so no need to check for characters
+    with open(csv_file, 'r', newline = '') as f:
+        reader = csv.reader(f)
+        header = next(reader, None)
+
+        if header == None:
+            return False
+        return True
+
+#There is other content such as "banned tracks", "banned artists", and "other"
+#but none came with my spotify data, so I don't know the structure of the object
+#feel free to add it if you know said structure.
+
+
+
+
+#Add the path of the files to convert here
+folder_path = "/path/to/folder/my_spotify_data/MyData"
+
+#Creating a list of all the filenames in the folder:
+files = os.listdir(folder_path)
+
+#Converting for all the files called Playlist (previous version just converted a single file)
+#Same for all the files called library (my own spotify data only contained one; just in case.)
+#And for all the files of your streaming history.
+
+for file_name in files:
+    if file_name.startswith("YourLibrary"):
+        library_converter(os.path.join(folder_path, file_name), 'library.csv')
+
+for file_name in files:
+    if file_name.startswith("Playlist"):
+        playlist_converter(os.path.join(folder_path, file_name), 'playlist.csv')
+
+for file_name in files:
+    if file_name.startswith("StreamingHistory"):    
+        history_converter(os.path.join(folder_path, file_name), 'history.csv')
+
